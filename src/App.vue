f2e2c1736f3e620c23e2899f813a278be660805b
Zulfidly Zulkifli <zulfidlyzulkifli@QMPC082.local>
2023-10-13 11:01:17 +0800

    test-n01

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import TheWelcome from './components/TheWelcome.vue'
   <header>
     <h1>commit & push branch-test-01</h1>
     <h1>git reset --hard e621ec2</h1>
+    <h1>test-n01</h1>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
