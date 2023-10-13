e621ec20efa974eac490312f5af848504df84dc6
Zulfidly Zulkifli <zulfidlyzulkifli@QMPC082.local>
2023-10-13 10:05:59 +0800

    test-01

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
+    <h1>commit & push branch-test-01</h1>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
